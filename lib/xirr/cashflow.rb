1643d31c1ada07ed6210329f8b47e14a4d96c450
jstarner <justin@polarnotion.com>
2018-07-25 18:09:31 -0400

    Update cashflow.rb

--- lib/xirr/cashflow.rb
+++ lib/xirr/cashflow.rb
@@ -41,7 +41,8 @@ module Xirr
     # Last investment date
     # @return [Time]
     def max_date
-      @max_date ||= self.map(&:date).max
+      @max_date ||= self.last.date
+#       @max_date ||= self.map(&:date).max
     end
 
     # Calculates a simple IRR guess based on period of investment and multiples.
@@ -102,7 +103,8 @@ module Xirr
     # First investment date
     # @return [Time]
     def min_date
-      @min_date ||= self.map(&:date).min
+      @min_date ||= self.first.date
+#       @min_date ||= self.map(&:date).min
     end
 
     # @return [String]
